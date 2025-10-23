7030a0484b48c8bc7ecbbe424c496ce64eadc87a
qright <antonio.santak@fer.unizg.hr>
2025-10-23 21:58:06 +0200

    Jednostavno čitanje iz baze podataka

--- src/main/java/hr/fer/progi/autoservis/controller/MainController.java
+++ src/main/java/hr/fer/progi/autoservis/controller/MainController.java
@@ -1,24 +1,27 @@
 package hr.fer.progi.autoservis.controller;
 
+import hr.fer.progi.autoservis.model.TestingModel;
+import hr.fer.progi.autoservis.repository.TestingRepository;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class MainController {
 
-    private final BuildProperties build_properties;
+    private final TestingRepository repo;
 
-    public MainController(BuildProperties buildProperties){
-        this.build_properties = buildProperties;
+    public MainController(TestingRepository repository){
+        this.repo = repository;
     }
 
     @GetMapping("/page")
     public String page(Model model){
-        String message = "THIS IS DYNAMIC TEXT";
-        model.addAttribute("message", message);
-        model.addAttribute("buildNumber", build_properties.getVersion());
+        List<TestingModel> list = repo.findAll();
+        model.addAttribute("services", list);
 
         return "dynamic";
     }
